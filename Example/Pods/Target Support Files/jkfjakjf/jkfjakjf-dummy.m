06ef4c645aa13e0daf387bf50ceb19ef6a28388d
chen <chen_zhengwen@want-want.com>
2021-11-18 10:39:42 +0800

    first commit

---
+++ Example/Pods/Target Support Files/jkfjakjf/jkfjakjf-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_jkfjakjf : NSObject
+@end
+@implementation PodsDummy_jkfjakjf
+@end
---
+++ jkfjakjf/Classes/NSObject+dd.m
@@ -0,0 +1,17 @@
+//
+//  NSObject+dd.m
+//  RecordVoiceDemo
+//
+//  Created by 陈正文 on 2021/11/15.
+//  Copyright © 2021 allison. All rights reserved.
+//
+
+#import "NSObject+dd.h"
+
+@implementation NSObject (dd)
+
+- (void)printName{
+    NSLog(@"%s", __func__);
+}
+
+@end
